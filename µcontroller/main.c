f78551ba10176508538f52519e69e8703713487d
sandra <sandyguefy@gmail.com>
2023-03-19 14:54:25 +0100

    Video of the first project Video of the first project

---
+++ µcontroller/main.c
@@ -0,0 +1,56 @@
+
+/*
+problem : 
+Detect if user press USER_BUTTON
+Toggle GPIOD_12 (PD12) LED at some visible interval
+
+Each of the GPIO pins can be configured by software as output (push-pull or open-drain, with or without pull-up or pull-down), as input (floating, with or without pull-up or pull-down)
+GPIO -> OUTPUT[PUSH_pULL | open drain]
+// ---configurations---
+GPIOx_MODER,
+GPIOx_OTYPER, 
+GPIOx_OSPEEDR
+and GPIOx_PUPDR
+
+
+GPIOx_IDR  
+GPIOx_ODR 
+GPIOx_BSRR
+
+GPIOx_AFRH
+GPIOx_AFRL
+
+
+*/
+
+
+
+#include "stm32f407xx.h"
+#define LED_DELAY  1000000
+void delay (uint32_t x);
+void delay (uint32_t x){
+	for (;x>0;x--)
+	{
+		//__ASM ("NOP"); 
+	
+	}
+	
+}
+int main(){
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; //USER BUTTON -> PAO
+	
+	// GPIO12_12 AS OUTPUT-----
+ GPIOD->MODER |= (1<<24);
+ GPIOD->MODER &= ~(1<<25);
+ GPIOD->ODR &= ~(1<<12);
+	//-----------------------
+	while (1){
+		if (GPIOA->IDR & (1<<0)){
+		GPIOD->ODR ^=(1<<12);	
+		delay (LED_DELAY);
+		}
+	
+	}
+	
+}
